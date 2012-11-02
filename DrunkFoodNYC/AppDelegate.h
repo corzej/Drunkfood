d2a2df856fe674bf30fa9bd4e66e62ed655fa899
Eung Jin Lee <corzej@gmail.com>
2012-11-02 19:09:43 -0400

    Initial Commit

---
+++ DrunkFoodNYC/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  DrunkFoodNYC
+//
+//  Created by Eung Jin Lee on 11/2/12.
+//  Copyright (c) 2012 Eung Jin Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ DrunkFoodNYC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DrunkFoodNYC
+//
+//  Created by Eung Jin Lee on 11/2/12.
+//  Copyright (c) 2012 Eung Jin Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
